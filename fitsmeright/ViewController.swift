118aafc968b90b35ef45c6d710c9226dc2c25b42
lynnpark <lhnkki@gmail.com>
2018-12-08 15:14:18 +0700

    [fix]: Create project

---
+++ fitsmeright/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitsmeright
+//
+//  Created by Lynn Park on 8/12/2561 BE.
+//  Copyright © 2561 silpakorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
