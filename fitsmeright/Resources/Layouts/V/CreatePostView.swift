045df5c270807b7a7ecd189a83ca07f279a87f09
lynnpark <lhnkki@gmail.com>
2018-12-14 20:36:56 +0700

    [minor]: Create CreatePostView

--- fitsmeright/Resources/Layouts/V/CreatePostView.swift
+++ fitsmeright/Resources/Layouts/V/CreatePostView.swift
@@ -20,85 +20,96 @@ extension CreatePostView
 
 
 class CreatePostView: UIView {
-
+  
   //MARK: - UI
   @IBOutlet weak var imgv_01: UIImageView!
   @IBOutlet weak var imgv_02: UIImageView!
   @IBOutlet weak var imgv_03: UIImageView!
   @IBOutlet weak var imgv_04: UIImageView!
+  @IBOutlet weak var imgv_background: UIImageView!
   
-
-
+  
+  
   //MARK: - NSLayout
-
-
-
+  
+  
+  
   //MARK: - Constraint
-
-
-
+  
+  
+  
   //MARK: - Instance
-
-
-
+  
+  
+  
   //MARK: - Flag
-
-
-
+  
+  
+  
   //MARK: - Storage
-
-
-
+  
+  
+  
   //MARK: - Initial
-
-
-
+  
+  
+  
   //MARK: - Life cycle
   override func awakeFromNib() {
     super.awakeFromNib()
+    setupViewOnAwakeFromNib()
+  }
+  
+  
+  
+  //MARK: - Setup View
+  func setupViewOnAwakeFromNib() {
+    setupUI()
+    setupSnp()
+  }
+  
+  func setupUI() {
     imgv_01.contentMode = .scaleAspectFit
     imgv_02.contentMode = .scaleAspectFit
     imgv_03.contentMode = .scaleAspectFit
     imgv_04.contentMode = .scaleAspectFit
+    imgv_background.contentMode = .scaleAspectFit
   }
-
-
-
-  //MARK: - Setup View
-  func setupViewOnViewDidLoad() {
+  
+  func setupSnp() {
     
   }
-
-
-
+  
+  
+  
   //MARK: - Setup Data
-
-
-
+  
+  
+  
   //MARK: - Event
-
-
-
+  
+  
+  
   //MARK: - Public
-
-
-
+  
+  
+  
   //MARK: - Private
-
-
-
+  
+  
+  
   //MARK: - Core - Protocol
-
-
-
+  
+  
+  
   //MARK: - Custom - Protocol
-
-
-
+  
+  
+  
   //MARK: - Pods - Protocol
-
-
-
+  
+  
+  
 }
 
 
