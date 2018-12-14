66e68051a913d096e732d854fd6c2ece6abb1b59
lynnpark <lhnkki@gmail.com>
2018-12-15 01:27:19 +0700

    [minor]: PostVC update button

--- fitsmeright/Resources/Layouts/V/PostView.swift
+++ fitsmeright/Resources/Layouts/V/PostView.swift
@@ -121,10 +121,11 @@ class PostView: UIView {
   
   //MARK: - Setup Data
   func setupPostData(post: MockPost) {
-    let bkk = Region(calendar: Calendars.buddhist, zone: Zones.asiaBangkok, locale: Locales.thai)
-    let date = post._string_createdAt.toDate("yyyy-MM-dd HH:mm", region: bkk)
+//    let bkk = Region(calendar: Calendars.buddhist, zone: Zones.asiaBangkok, locale: Locales.thai)
+//    let date = post._string_createdAt.toDate("yyyy-MM-dd HH:mm", region: bkk)
+//    let date = post._string_createdAt.toFormat("dd MMM", locale: .thai)
     lb_username.text = "lin-ping"
-    lb_createdAt.text = date?.toString() ?? ""
+    lb_createdAt.text = post._string_createdAt
     lb_caption.text = post.string_captionSelected
     v_createPost.setupClothData(images: post.img_clothSelected)
     v_createPost.setupBackgroundData(image: post._img_backgroundSelected)
