0146bb8557efdda87aa12eb965a7e2c4afc5a4e1
ssankosik <aimangtub.mond@gmail.com>
2019-02-22 19:38:24 +0700

    [migrate]: AppDelegate

---
+++ fitsmeright/Classe/Extension/UIKit/AppDelegate/AppDelegate+Router.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate+Router.swift
+//  fitsmeright
+//
+//  Created by Sasawat Sankosik on 22/2/2562 BE.
+//  Copyright © 2562 silpakorn. All rights reserved.
+//
+
+
+
+import SwifterSwift
+
+
+
+extension AppDelegate {
+  
+  //MARK: - Implement
+  
+  
+  
+  //MARK: - Public
+  func route(with launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
+    let vc = SplashVC.vc()
+    vc.launchOptions = launchOptions
+    window?.set(with: vc, style: .fade)
+  }
+  
+  func route(with app: UIApplication, url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+    guard let scheme = url.scheme else {
+      return false
+    }
+    if scheme.isEqual("") {
+      return false
+    }
+    //    if SDKApplicationDelegate.shared.application(app, open: url, options: options) {
+    //      return true
+    //    }
+    //    if GIDSignIn.sharedInstance().handle(url, sourceApplication: options[.sourceApplication] as? String, annotation: [:]) {
+    //      return true
+    //    }
+    //Handle Deeplink
+    return false
+  }
+  
+  
+  
+  //MARK: - Private
+  
+}
--- fitsmeright/Main/AppDelegate.swift
+++ fitsmeright/Main/AppDelegate.swift
@@ -8,14 +8,19 @@
 
 
 
-import UIKit
+import SwifterSwift
+import IQKeyboardManagerSwift
+import Firebase
+import PopupDialog
+import SwifterSwift
+import Kingfisher
 
 
 
 extension AppDelegate:
   UIApplicationDelegate,
-  AGApplicationSetup,
-  AGApplicationRoutable
+  Preparable,
+  Colorable
 { }
 
 
@@ -49,23 +54,204 @@ class AppDelegate: UIResponder {
   
   
   //MARK: - Storage
-  static var app_delegate: UIApplicationDelegate? {
-    return UIApplication.shared.delegate
-  }
   
-  static var window: UIWindow? {
-    return app_delegate?.window ?? nil
-  }
   
+  
+  //MARK: - Apperance
+  
+  
+  
+  //MARK: - Initial
   override init() {
     window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
     super.init()
-    setupOnInit()
+    setupInit()
   }
   
   deinit {
-    setupOnDeinit()
+    setupDeinit()
   }
+  
+  func setupInit() {
+    //MARK: Core
+    
+    
+    
+    //MARK: Component
+    //    SwifterSwift.appVersion
+    //    FMUserDefaults.removeAll()
+    let fsUser = FSUser()
+    fsUser.documentId = "N5GUixP95dAMKzXBx5kG"
+    //        fsUser.documentId = "VceACbsT1S2QPNV3JQS7"
+    fsUser.email = "lhnkki@gmail.com"
+    fsUser.username = "lin999"
+    fsUser.displayName = "linping"
+    fsUser.password = "1234"
+    fsUser.bio = "hi it's me"
+    FMUserDefaults.FSUserDefault.set(data: fsUser)
+    
+    
+    
+    //MARK: Other
+    
+    
+    
+    //MARK: Snp
+    
+    
+    
+    //MARK: Localize
+    
+    
+    
+    //MARK: Data
+  }
+  
+  func setupPrepare() {
+    
+  }
+  
+  func setupDeinit() {
+    
+  }
+  
+  
+  
+  //MARK: - LifeCycle
+  
+  
+  
+  //MARK: - SetupView
+  func setupOnDidFinishLaunching(with app: UIApplication, options: [UIApplication.LaunchOptionsKey: Any]?) {
+    
+    func setupCores(with app: UIApplication, options: [UIApplication.LaunchOptionsKey: Any]?) {
+      //MARK: UIApplication
+      
+      
+      
+      //MARK: UIBarButtonItem
+      let nb = UINavigationBar.appearance()
+      nb.setupWith(content: .white, bg: c_custom.peach, isTranslucent: false)
+      
+      //MARK: UIBarButtonItem
+      let bbi = UIBarButtonItem.appearance()
+      bbi.setBackButtonTitlePositionAdjustment(UIOffset(horizontal: -300, vertical: 0.0), for: .default)
+      
+      //MARK: UITabBar
+      let tb = UITabBar.appearance()
+      tb.setupWith(content: .white, bg: c_custom.peach, isTranslucent: false)
+      
+      //MARK: UITextField
+      let txt = UITextField.appearance()
+      txt.setupDark()
+      
+      //MARK: UIBarItem
+      //    let bi = UIBarItem.appearance()
+    }
+    
+    func setupPods(with app: UIApplication, options: [UIApplication.LaunchOptionsKey: Any]?) {
+      //MARK: Firebase
+      FirebaseApp.configure()
+      
+      
+      
+      //MARK: IQKeyboardManagerSwift
+      let iqkbm = IQKeyboardManager.shared
+      iqkbm.enable = true
+      iqkbm.placeholderFont = UIFont.preferredFont(forTextStyle: .body)
+      iqkbm.shouldToolbarUsesTextFieldTintColor = true
+      iqkbm.overrideKeyboardAppearance = true
+      iqkbm.shouldResignOnTouchOutside = true
+      iqkbm.toolbarDoneBarButtonItemText = "Done"
+      //    iqkbm.toolbarManageBehaviour = .byPosition
+      //    iqkbm.previousNextDisplayMode = .alwaysShow
+      
+      //    @objc public var toolbarTintColor : UIColor?
+      //
+      //    /**
+      //     This is used for toolbar.barTintColor. Default is nil and uses white color.
+      //     */
+      //    @objc public var toolbarBarTintColor : UIColor?
+      
+      
+      
+      //MARK: PopupDialog
+      let pddv = PopupDialogDefaultView.appearance()
+      pddv.backgroundColor = .white
+      //    pddv.titleFont = .boldSystemFont(ofSize: 14)
+      //    pddv.titleColor = UIColor(white: 0.4, alpha: 1)
+      //    pddv.titleTextAlignment = .center
+      //    pddv.messageFont = .systemFont(ofSize: 14)
+      //    pddv.messageColor = UIColor(white: 0.6, alpha: 1)
+      //    pddv.messageTextAlignment = .center
+      
+      let pdcv = PopupDialogContainerView.appearance()
+      pdcv.backgroundColor = UIColor(red:0.23, green:0.23, blue:0.27, alpha:1.00)
+      //    pdcv.cornerRadius = 2
+      //    pdcv.shadowEnabled = true
+      //    pdcv.shadowColor = c_material.grey300
+      //    pdcv.shadowOpacity = 0.6
+      //    pdcv.shadowRadius = 20
+      //    pdcv.shadowOffset = CGSize(width: 0, height: 8)
+      //    pdcv.shadowPath = CGPath(...)
+      
+      let pdov = PopupDialogOverlayView.appearance()
+      pdov.color = c_material.grey300
+      //    pdov.blurRadius = 20
+      //    pdov.blurEnabled = true
+      //    pdov.liveBlurEnabled = false
+      //    pdov.opacity = 0.7
+      
+      let db = DefaultButton.appearance()
+      db.titleFont = .systemFont(ofSize: 14)
+      //    db.titleColor = UIColor(red: 0.25, green: 0.53, blue: 0.91, alpha: 1)
+      //    db.buttonColor = .clear
+      //    db.separatorColor = UIColor(white: 0.9, alpha: 1)
+      
+      let cb = CancelButton.appearance()
+      cb.titleFont = .systemFont(ofSize: 14)
+      //    cb.titleColor = UIColor(red: 0.25, green: 0.53, blue: 0.91, alpha: 1)
+      //    cb.buttonColor = .clear
+      //    cb.separatorColor = UIColor(white: 0.9, alpha: 1)
+      
+      
+      
+      //MARK: Kingfisher
+      let kf = KingfisherManager.shared
+      kf.defaultOptions = [.transition(.fade(1))]
+      // Limit memory cache size to 300 MB.
+      kf.cache.memoryStorage.config.totalCostLimit = 300 * 1024 * 1024
+      // Limit memory cache to hold 150 images at most.
+      kf.cache.diskStorage.config.expiration = .seconds(1000 * 1024 * 1024)
+      kf.cache.diskStorage.config.sizeLimit = 600
+      
+    }
+    
+    setupCores(with: app, options: options)
+    setupPods(with: app, options: options)
+    
+  }
+  
+  
+  
+  //MARK: - SetupData
+  
+  
+  
+  //MARK: - Event
+  
+  
+  
+  //MARK: - Public
+  
+  
+  
+  //MARK: - Private
+  
+  
+  
+  //MARK: - VIP - UseCase
   
   
   
@@ -73,7 +259,7 @@ class AppDelegate: UIResponder {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     setupOnDidFinishLaunching(with: application, options: launchOptions)
-    start(with: launchOptions, condition: FMUserDefaults.LoggedIn.get() ?? false)
+    route(with: launchOptions)
     return true
   }
   
@@ -97,9 +283,18 @@ class AppDelegate: UIResponder {
   
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    
   }
   
+  
+  
+  //MARK: - Custom - Protocol
+  
+  
+  
+  //MARK: - Pod - Protocol
+  
+  
+
 }
 
 
